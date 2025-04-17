d2aa5b47749e104ce24fcb9c52b197f58590e664
Fabio Felici <fabio.felici@pagopa.it>
2025-04-17 15:48:04 +0200

    Restrict artifact upload to Python 3.12 only
    Added a condition to the artifact upload step to run only for This avoids unnecessary uploads for other Python versions, optimizing the workflow process.

--- src/certdiff/core.py
+++ src/certdiff/core.py
@@ -1,6 +1,5 @@
 import json
 from cryptography import x509
-from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from rich.console import Console
 
@@ -10,18 +9,21 @@ console = Console()
 def load_certificate(cert_path):
     with open(cert_path, 'rb') as f:
         cert_data = f.read()
-        return x509.load_pem_x509_certificate(cert_data, default_backend())
+        return x509.load_pem_x509_certificates(cert_data)
 
 
-def extract_certificate_info(cert):
-    return {
-        "subject": cert.subject.rfc4514_string(),
-        "issuer": cert.issuer.rfc4514_string(),
-        "serial_number": hex(cert.serial_number),
-        "not_valid_before": cert.not_valid_before_utc.isoformat(),
-        "not_valid_after": cert.not_valid_after_utc.isoformat(),
-        "fingerprint_sha256": cert.fingerprint(hashes.SHA256()).hex()
-    }
+def extract_certificate_info(certs):
+    certs_info = []
+    for cert in certs:
+        certs_info.append({
+            "subject": cert.subject.rfc4514_string(),
+            "issuer": cert.issuer.rfc4514_string(),
+            "serial_number": hex(cert.serial_number),
+            "not_valid_before": cert.not_valid_before_utc.isoformat(),
+            "not_valid_after": cert.not_valid_after_utc.isoformat(),
+            "fingerprint_sha256": cert.fingerprint(hashes.SHA256()).hex(),
+        })
+    return certs_info
 
 
 def compare_certificates(old_cert_input, new_cert_input, verbose=False, report_file=None):
@@ -42,23 +44,29 @@ def compare_certificates(old_cert_input, new_cert_input, verbose=False, report_f
 
     if verbose:
         console.print("[bold cyan]🔍 Verbose mode enabled.[/bold cyan]")
-        console.print("[blue]Old certificate info:[/blue]")
-        for key, value in old_info.items():
-            console.print(f"  {key}: {value}")
+        console.rule(title="[blue]Old certificate info:[/blue]")
+        for o in old_info:
+            console.rule(o["issuer"])
+            for key, value in o.items():
+                console.print(f"  {key}: {value}")
 
-        console.print("[magenta]New certificate info:[/magenta]")
-        for key, value in new_info.items():
-            console.print(f"  {key}: {value}")
+        console.rule(title="[magenta]New certificate info:[/magenta]")
+        for n in new_info:
+            console.rule(n["issuer"])
+            for key, value in n.items():
+                console.print(f"  {key}: {value}")
 
     fields_to_compare = ["subject", "issuer"]
 
     for field in fields_to_compare:
-        if old_info[field] != new_info[field]:
-            differences.append({
-                "field": field,
-                "old": old_info[field],
-                "new": new_info[field]
-            })
+        for cert_count in range(max(len(old_info), len(new_info))):
+            cert_count=cert_count-1
+            if old_info[cert_count][field] != new_info[cert_count][field]:
+                differences.append({
+                    "field": field,
+                    "old": old_info[cert_count][field],
+                    "new": new_info[cert_count][field]
+                })
 
     if report_file:
         report_data = {
